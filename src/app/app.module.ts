bc59d5cfeb7436549c96f4a240e66f9861546256
Jefferson Restani <jeffersonrestani@gmail.com>
2023-04-25 21:54:15 -0300

    feat - create customer component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CutomerComponent } from './customer/customer.component';
+import { CutomerService } from './customer.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CutomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MatButtonModule,
+    MatDividerModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    CutomerService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
